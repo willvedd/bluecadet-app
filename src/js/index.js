e146f0538b3bad5cb0c8140e1b52c0084f294d15
Williamson Vedder <will@bluecadet.com>
2016-03-15 01:31:57 -0400

    Step 2 Developing out

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,37 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+
+var BCapp = React.createClass({displayName: "BCapp",
+  getInitialState:function(){
+    return {
+      hightlight: false,
+    }
+  },
+
+  toggleState:function(){
+	this.setState({
+  		hightlight: !this.state.hightlight,
+  	});
+  },
+
+  isHighlighted:function(){
+  	return (this.state.hightlight == true);
+  },
+
+  render: function() {
+  	var self = this;
+  	console.log("render",this.state);
+    return (
+      <div>
+      	<h1 className={self.isHighlighted() ? 'highlighted' :''}><i>The</i> Official Bluecadet App</h1>
+      	<button onClick={()=>self.toggleState()}>{self.isHighlighted()?  'Unhighlight':'Highlight'}</button>
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  <h1>The Official Bluecadet app</h1>,
-  document.getElementById('app')
+	<BCapp></BCapp>,
+  	document.getElementById('app')
 );
